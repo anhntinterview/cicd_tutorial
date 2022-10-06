96671304348b741d5208b99df3e4849c9d560806
anhntinteview <roberthost.vn@gmail.com>
2022-10-06 11:56:26 +0700

    Add 'Test' stage

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Tang tang tang, tanng tang tang tang tang tang/i);
   expect(linkElement).toBeInTheDocument();
 });
